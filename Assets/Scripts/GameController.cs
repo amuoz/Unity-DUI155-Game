da99d735794511ec8073813b75a4e1712fd97726
amunoz <antmunozga@gmail.com>
2017-10-24 15:14:49 +0200

    GameController init

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    // referencia a prefab de la caja
+    public GameObject boxPrefab;
+    public int tileSize = 32;
+
+	// Use this for initialization
+	void Start () {
+        GameObject boxInstance = Instantiate(boxPrefab);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
